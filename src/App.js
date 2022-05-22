5cc113793bf8aa60e1768c15f8c43bdb134e6d2d
nawiq7208 <36619633+nawiq7208@users.noreply.github.com>
2022-05-22 22:09:24 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+import Test from "./pages/test.js";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <p>Sebuah Paragraf Baru</p>
+      <Test />
+    </div>
+  );
+}
---
+++ src/pages/test.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Test() {
+  return <div>hej hej</div>;
+}
+
+export default Test;
